4770a87bb90ee714de322a5fb8ece40cb1a63da2
Axel Eschholz <axelion737@gmail.com>
2021-11-16 19:48:18 -0400

    Implements encryption

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function CheckPassword(inputtxt) {
-  var passw = "demondance";
-  if (inputtxt.match(passw)) {
-    window.location.href = "https://axeleschholz.github.io/demons";
+  var passw = -1783174268;
+  var hashed = hashpass(inputtxt);
+  if (hashed == passw) {
+    window.location.href = "demons";
   }
+}
+
+function hashpass(string) {
+  //set variable hash as 0
+  var hash = 732;
+  // if the length of the string is 0, return 0
+  if (string.length == 0) return hash;
+  for (i = 0; i < string.length; i++) {
+    ch = string.charCodeAt(i);
+    hash = (hash << 5) - hash + ch;
+    hash = hash & hash;
+  }
+  return hash;
 }
